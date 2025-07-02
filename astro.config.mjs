26eaa3d033ab1af26e3beeb656756b01b1614882
adwinugroho <adwinnugroho16@gmail.com>
2025-07-02 14:14:02 +0700

    feat: release cryppy with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import {
     defineConfig
 } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
+    site: 'https://cryppy.xyz',
     outDir: 'dist',
-    base: '/'
 });
